0feaec7e335841e7bcad7984830e23ed1d3186d5
mc_fdc <45692264+tuna2134@users.noreply.github.com>
2022-03-19 18:08:00 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,43 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+    
+def _requires_from_file(filename):
+    return open(filename, encoding="utf8").read().splitlines()
+  
+packages = ["shell"]
+
+extras = {
+    "speed": [
+        "ujson>=1.35",
+        "uvloop>=0.5.3"
+    ]
+}
+
+setuptools.setup(
+    entry_points={
+        "console_scripts": [
+            "venvx = shell:main"
+        ]
+    },
+    project_urls = {
+        "Documentation": "https://tuna2134.github.io/venvx/"
+    },
+    extras_require=extras,
+    packages=packages,
+    name="venvx",
+    version="0.0.1",
+    author="DMS",
+    author_email="masato190411@gmail.com",
+    description="venv wrapper",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/tuna2134/venvx",
+    install_requires=_requires_from_file('rqs.txt'),
+    classifiers=[
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
