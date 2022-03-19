50d8e8dd3a46f68cb04df1eabb1f1c7b4d81d3dd
mc_fdc <45692264+tuna2134@users.noreply.github.com>
2022-03-19 18:35:29 +0900

    Update __init__.py

--- shell/__init__.py
+++ shell/__init__.py
@@ -6,11 +6,11 @@ def check_venv():
     if path.isdir("./venv"):
         pass
     else:
-        subprocess.Popen(["python3", "-m", "venv", "venv"])
+        subprocess.run(["python3", "-m", "venv", "venv"], stdout=subprocess.PIPE)
 
 def main():
     check_venv()
     sub = argv[1]
     if sub == "install":
-        subprocess.Popen(["venv/bin/pip", "install", argv[1:]], stdout=subprocess.PIPE)
+        subprocess.run(["venv/bin/pip", "install", argv[1:]], stdout=subprocess.PIPE)
         print("インストール完了！")
