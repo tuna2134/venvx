51b19917be2e60e28cb16a78749883ecec7b4f82
mc_fdc <45692264+tuna2134@users.noreply.github.com>
2022-03-19 19:26:42 +0900

    Update __init__.py

--- shell/__init__.py
+++ shell/__init__.py
@@ -12,10 +12,10 @@ def main():
     check_venv()
     sub = argv[1]
     if sub == "install":
+        print(argv[1:])
         subprocess.run(["./venv/bin/pip", "install"] + argv[1:], stdout=subprocess.PIPE)
         print("実行完了！")
     elif sub == "uninstall":
-        print(argv[1:])
         p = subprocess.Popen(["./venv/bin/pip", "uninstall"] + argv[2:], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
         p.stdin.write(b'y')
         p.stdin.close()
