7003a3c4937725b019f6cd0815241d2d366b3b12
mc_fdc <45692264+tuna2134@users.noreply.github.com>
2022-04-22 21:45:55 +0900

    Create pip.py

---
+++ venvx/pip.py
@@ -0,0 +1,26 @@
+from os import path
+import subprocess
+from venv import EnvBuilder
+
+projects_dir = path.join(path.abspath(path.dirname(__name__)), 'projects')
+
+class PipEnv:
+    def __init__(self, name, libs):
+        self.env = EnvBuilder(clear=True, with_pip=True)
+        if not path.isdir(f"{projects_dir}/{name}"):
+            self.env.create(f"{projects_dir}/{name}")
+        self.pip = f"{projects_dir}/{name}/bin/pip"
+        
+    def do_pip(self, *args, **kwargs):
+        subprocess.run([self.pip] + list(args),
+                       stdout=subprocess.PIPE,
+                       stderr=subprocess.STDOUT,
+                       **kwargs)
+            
+    def install(self, name, *args):
+        self.do_pip(["install", name] + args)
+        print("Complete")
+        
+    def uninstall(self, name, *args):
+        self.do_pip(["uninstall", name, "-y"] + args)
+        print("Complete")
