60c377eb5bd4952fe4be1e1f77706d19ab34ea7b
mc_fdc <45692264+tuna2134@users.noreply.github.com>
2022-04-22 21:17:48 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ packages = ["shell"]
 setuptools.setup(
     entry_points={
         "console_scripts": [
-            "venvx = shell:main"
+            "venvx = venvx:main"
         ]
     },
     project_urls = {
